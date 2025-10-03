fb3f4f289bf0c046b5a2a0dd6c3be3c9a2a9bd53
IvanF2006 <fuentesivanm006@gmail.com>
2025-10-03 13:59:16 -0600

    Charla sobre cyber security
    Lo que escribi de aha la ciber seguridad type shi

---
+++ ClasesISDtypeshi.py
@@ -0,0 +1,20 @@
+"""Charla Motivacional sobre la ciber seguiridad type shi
+Va por ejemplo aha estan los deepfakes que son basciamente todas las cuestiones donde te pueden clonar, ya sea la voz
+o la cara
+
+Otro ejemplo son los chatbots en donde puede ser usados o manipulados para hacer creer que estan hablando con alguien real
+o que lo que le estan diciendo es legit o los videos hechos con IA y toda esa bs
+
+Cada organizacion depende de la integridad digital para operar, y las amenzas son constantes sofisticadas y con alto impacto 
+operativo y aha ya que sus riesgos son heavy como suplantacion de indentidad, robo de propiedad intelectual y secretos
+indistriales, tambien pueden suceder interrupciones operativas, servicios no disponibles para los clientes, fallos internos,
+etc, sus consecuencias pueden ser el daño a la reputacion e imagen, perdida de confianza en los clientes, sanciones legales y
+multas, perdidas economicas, afectacion de sistemas e informacion, cierre total o parcial de operaciones.
+
+Bueno habala tambien de como indentificar los deepfakes, y bueno nomas es como fijarse en pequeños detalles y nomas ver las
+cosas con tus propios ojos y no solo ver por ver y tal
+
+Bueno tambien en general basic shi como ocultar tu identidad en las redes y tal, desconfiar de todos incluso de tu madre pq
+aha, otra cosa tambien es no usar cualquier usb que uno encuentre asi a lo loco
+
+Y bueno tambien esta las weas de aha no descargar cualquier wea pq no sabi lo que la wea te puede wear wn"""
